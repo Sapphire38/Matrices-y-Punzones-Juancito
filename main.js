20ea53a8af753eea95c41155fec8ddc6e9ad3560
Juani <juanygr@hotmail.com>
2023-04-21 16:35:24 -0300

    update

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
+const menu = document.getElementById("menu-b")
+
 document.getElementById("collapce").addEventListener('click', abrirmenu)
 
-function abrirmenu(){
-    document.getElementById("menu-b").style.display="flex"
+function abrirmenu() {
+    menu.style.display = "flex"
 }
 
 document.getElementById("close-collapce-button").addEventListener('click', cerrarMenu)
 
-function cerrarMenu(){
-    document.getElementById("menu-b").style.display="none"
+function cerrarMenu() {
+    menu.style.display = "none"
 }
-var h 
+var h
 
-window.onload = function (){
-     h = window.innerHeight
-     h = (h) +"px"
-     console.log(h)
-     document.getElementById("head").style.height = h
-     document.getElementById("card-form").style.height = h - (h * 0.2)
-    }
+window.onload = function () {
+    h = window.innerHeight
+    h = (h) + "px"
+    console.log(h)
+    document.getElementById("head").style.height = h
+    document.getElementById("card-form").style.height = h - (h * 0.6)
+    document.getElementById("contain-btns").style.height = h - (h * 0.6)
+}
 
+function popUp(){
+    console.log(h) 
+}
---
+++ validateform.js
@@ -0,0 +1,57 @@
+function validateForm() {
+    event.preventDefault()
+   var name =  document.getElementById('name').value;
+   if (name == "") {
+       document.getElementById('status').innerHTML = "Ingresa tu nombre.";
+       return false;
+   }
+   var subject =  document.getElementById('subject').value;
+   if (subject == "") {
+       document.getElementById('status').innerHTML = "Ingresa un asunto.";
+       return false;
+   }
+
+   var number =  document.getElementById('number').value;
+   if (number == "") {
+       document.getElementById('status').innerHTML = "Ingresa tu numero celular";
+       return false;
+   }
+   var message =  document.getElementById('message').value;
+   if (message == "") {
+       document.getElementById('status').innerHTML = "Informanos sobre tu consulta";
+       return false;
+   }
+   
+   document.getElementById('status').innerHTML = "";
+
+
+   document.getElementById('btn-send').setAttribute('href', `mailto:lucasdiforte@gmail.com?subject=${subject}&body=Hola soy ${name}! %0D%0AMi consulta: "${message}" %0D%0ANumero de telefono: ${number}`);
+
+   document.getElementById('btn-send').click();
+   
+
+   };
+
+
+
+   function validateNews() {
+    var email =  document.getElementById('email-news').value;
+    if (email == "") {
+        document.getElementById('status-news').innerHTML = "Ingresa tu email.";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.getElementById('status-news').innerHTML = "Email incorrecto.";
+            return false;
+        }
+    }
+    
+
+    document.getElementById('btn-send').setAttribute('href', `mailto:lucasdiforte@gmail.com?subject=Newsletter&body=Hola, me gustaria que me mantengan al día con nuestras últimas empresas y noticias.`);
+
+    document.getElementById('btn-send').click();
+    
+ 
+    };
+ 
